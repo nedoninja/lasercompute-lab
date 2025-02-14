7f8a7406db5206f0b7a8d78d75586daf37309b4a
Timofei Gorlov <72683799+TiM-SyStEm@users.noreply.github.com>
2025-02-14 22:48:43 +0300

    COM-port connection, accuracy parse

---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaserComputeLab
+{
+    public partial class Form1 : Form
+    {
+        private ManagerCOM mcom;
+        private SerialPort sp;
+        private static string databuffer = String.Empty;
+        private static string databuffer_add = String.Empty;
+        private int mode = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            panelBase.Visible = true;
+        }
+
+        private void labindex_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (labindex.SelectedIndex == 0) {
+                panelBase.Visible = true;
+                panel0.Visible = false;
+            }
+            else if (labindex.SelectedIndex == 1) {
+                panel0.Visible = true;
+            }
+        }
+
+        private void managerCOM_Click(object sender, EventArgs e)
+        {
+            mcom = new ManagerCOM();
+            mcom.connectedStatus = connectedStatus;
+            sp = mcom.getPort();
+            sp.DataReceived += serialPort_DataReceived;
+            mcom.Show();
+        }
+
+        private void btnCorrect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!sp.IsOpen) sp.Open();
+                sp.WriteLine("!");
+                databuffer_add = String.Empty;
+                mode = 1;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не получилось отправить данные на порт " + mcom.getPortname(), "Ошибка отправки данных");
+            }
+        }
+        private async void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (mcom != null)
+            {
+                string data = mcom.getDataReceived();
+                databuffer = data;
+                Invoke(new Action(() => databuffer_add += databuffer));
+                Invoke(new Action(() => debugInput.Text += databuffer));
+                if (mode == 1)
+                {
+                    // Режим корректировки
+                    if (databuffer_add.Contains("Autocompute [done]\r\n"))
+                    {
+                        // Начинаем парсить данные
+                        string[] lines = databuffer_add.Split('\n');
+                        string floor = lines[1].Split(' ')[1];
+                        string roof = lines[2].Split(' ')[1];
+                        Invoke(new Action(() => floor1.Text = floor));
+                        Invoke(new Action(() => roof1.Text = roof));
+                    }
+                }
+            }
+        }
+        private void clearDebug_Click(object sender, EventArgs e)
+        {
+            debugInput.Text = String.Empty;
+        }
+
+        private void debugToolStrip_CheckedChanged(object sender, EventArgs e)
+        {
+            ToolStripMenuItem menuItem = sender as ToolStripMenuItem;
+            if (menuItem.CheckState == CheckState.Checked)
+                groupDebug.Visible = true;
+            else if (menuItem.CheckState == CheckState.Unchecked)
+                groupDebug.Visible = false;
+        }
+    }
+}
---
+++ ManagerCOM.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaserComputeLab
+{
+    public partial class ManagerCOM : Form
+    {
+        public static bool isConnectedPort = false;
+        private string portname = String.Empty;
+        public ToolStripLabel connectedStatus;
+        private string datareceived = String.Empty;
+        public ManagerCOM()
+        {
+            InitializeComponent();
+        }
+
+        private void scanpotrs_Click(object sender, EventArgs e)
+        {
+            portselector.Items.Clear();
+            string[] portnames = SerialPort.GetPortNames();
+            if (portnames.Length == 0)
+            {
+                MessageBox.Show("Нет доступных COM-портов", "Ошибка подключения");
+            }
+            else portselector.Enabled = true;
+            foreach (string portName in portnames)
+            {       
+                portselector.Items.Add(portName);
+                if (portnames[0] != null)
+                {
+                    portselector.SelectedItem = portnames[0];
+                }
+            }
+        }
+
+        private void chooseport_Click(object sender, EventArgs e)
+        {
+            portname = portselector.GetItemText(portselector.SelectedItem);
+            try
+            {
+                serialPort.PortName = portname;
+                serialPort.Open();
+                isConnectedPort = true;
+                portdisconnect.Enabled = true;
+                chooseport.Enabled = false;
+                speedselector.Enabled = true;
+                connectedStatus.Text = "Подключено " + portname;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Невозможно подключиться к порту " + portname + ", проверьте подключено ли устроство или закройте программы, которые его также используют.", "Ошибка доступа к порту");
+            }
+            catch (System.IO.IOException)
+            {
+                MessageBox.Show("Этого порта не существует (" + portname + "), проверьте подключение устройства.", "Ошибка доступа к порту");
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Не удалось подключиться к " + portname + " по неизвестной причине.", "Ошибка доступа к порту");
+            }
+        }
+
+        private void portdisconnect_Click(object sender, EventArgs e)
+        {
+            serialPort.Close();
+            isConnectedPort = false;
+            portdisconnect.Enabled = false;
+            chooseport.Enabled = true;
+        }
+
+        private void speedselector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort.BaudRate = Convert.ToInt32(speedselector.SelectedText);
+        }
+        public SerialPort getPort()
+        {
+            return serialPort;
+        }
+        public string getPortname()
+        {
+            return portname;
+        }
+        public string getDataReceived()
+        {
+            return datareceived;
+        }
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            datareceived = serialPort.ReadExisting();
+        }
+    }
+}
